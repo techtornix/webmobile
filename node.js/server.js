6bc6400fa615625853b2f7bcc35841b94f8d37f0
muhammadbahawal <muhammadbahawalofficial@gmail.com>
2025-11-22 15:38:33 +0500

    updated

--- node.js/server.js
+++ node.js/server.js
@@ -1,29 +1,22 @@
 import express from 'express'
-
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("<h1>hello user</h1>")
+    res.send(`<h1> this is Home Page</h1>`)
 })
-
-// About Page (Params)
 app.get('/about/:userid', (req, res) => {
-    res.send(`<h1>hello user this is about page</h1> User ID: ${req.params.userid}`)
+
+    res.send(`this is about page ${req.params.userid}`)
 })
 
-// Contact Page (Multiple Params)
-app.get('/contact/:user1/:bookid', (req, res) => {
-    res.send(`<h1>hello user this is contact form</h1> 
-              User1: ${req.params.user1} <br>
-              BookID: ${req.params.bookid}`)
-})
 
-// Search Page (Query)
-app.get('/search', (req, res) => {
+app.get('/search',(req,res)=>{
+
     res.send(req.query)
 })
 
-app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
+
+app.listen(3000, () => {
+    console.log("Server is rining on port 3000")
+
 })
